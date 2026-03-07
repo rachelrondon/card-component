6414a8f234908bbeaada786d43957d44844f1d2b
Rachel <rachel.sun.rondon@gmail.com>
2026-03-07 18:26:28 -0500

    feat: style + create card component

---
+++ src/Card.js
@@ -0,0 +1,41 @@
+import './Card.css';
+import { ArrowDownCircleIcon, ArrowUpCircleIcon } from '@heroicons/react/24/outline';
+import { useState, useEffect } from 'react';
+
+const Card = () => {
+  const height = 10;
+  const expanded = height + 2.5;
+  
+  const [showMore, setShowMore] = useState(false);
+  const [cardHeight, setCardHeight] = useState(`${height}rem`);
+
+  useEffect(() => {
+      if (showMore) {
+        setCardHeight(`${expanded}rem`);
+      } else {
+        setCardHeight(`${height}rem`);
+      }
+  }, [showMore]);
+
+  const showMoreBtn = () => {
+    setShowMore(!showMore);
+  };
+
+  return (
+    <div className="card" style={{ height: cardHeight}}>
+      <div className="card-top"></div>
+      <hr />
+      {showMore ? (
+        <div className="card-bottom expanded">
+          <ArrowUpCircleIcon className="icon" onClick={showMoreBtn} />
+        </div>
+      ) : (
+        <div className="card-bottom">
+          <ArrowDownCircleIcon className="icon" onClick={showMoreBtn} />
+        </div>   
+      )}
+    </div>
+  )
+};
+
+export default Card;
---
+++ src/Show.js
@@ -0,0 +1,15 @@
+import Card from './Card.js';
+import './Show.css';
+
+const Show = () => {
+  
+  return (
+    <div className="layout">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+};
+
+export default Show;
